6a0e35cf1cf034bac26eaec7ba3918fa0a276588
Robert Renn <rrenn45@gmail.com>
2025-01-06 21:13:51 +0000

    removed unused CardDescription component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import {Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 
 
 export default function Home() {

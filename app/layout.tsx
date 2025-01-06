3f20b9ebd8a6f212e668b5438e09893d9ec548fc
Robert Renn <rrenn45@gmail.com>
2025-01-06 21:12:13 +0000

    removed unused fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Roboto, Lobster } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const roboto = Roboto({
   variable:'--font-roboto',
@@ -18,10 +10,7 @@ const roboto = Roboto({
   subsets:["latin"],
 })
 
-const lobster = Lobster({
-  weight:"400",
-  subsets:["latin"]
-})
+
 
 export const metadata: Metadata = {
   title: "R. Renn",

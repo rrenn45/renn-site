a052f57a99d0ced9f4cb93e2bd42e47ee31bd8a0
rrenn45 <rrenn45@gmail.com>
2025-01-19 12:27:16 -0800

    updated text color

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,6 @@ import Link from "next/link"
 
 
 export default function Home() {
-  return (<><h1 className="text-3xl my-2">Robert Renn</h1>
-    <p>I am experienced web developer using the following...</p>
-    <ul className="m-6 font-bold">
-      <li>- HTML, CSS, and JavaScript</li>
-      <li>- React</li>
-      <li>- Implementing React through the Next.js framework</li>
-    </ul>
-    <p>I started learning web development during the COVID lockdown.  I had experience with no-code application development, but decided to pursue the fundamental technology of the web.</p>
-    <p className="my-4 font-bold">Links:</p>
-    <ul className="flex flex-col p-4 max-w-24 justify-around border">
-      <li className="hover:font-bold"><Link href="https://github.com/rrenn45">GitHub</Link></li>
-      <li className="hover:font-bold"><Link href="https://www.linkedin.com/in/robert-renn-eit/">LinkedIn</Link></li>
-      
-    </ul>
-  </>)
+  return (<p>Robert Renn</p>
+  )
 }
